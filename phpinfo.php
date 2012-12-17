4aeffaabe1489f0779b1de6b75385ead31f7edaf
Li Guanghui <liguanghui@admaster.com.cn>
2012-12-17 15:40:04 +0800

    重新提交 Signed-off-by: Li Guanghui <liguanghui@admaster.com.cn>

---
+++ phpinfo.php
@@ -0,0 +1,6 @@
+<div align=center>phpStudy安装成功　　<a href="http://localhost/phpMyAdmin/" target="_blank">管理MySQL</a>　　<a href="http://www.phpStudy.net/php/" target="_blank">php教程</a>　　<a href="http://www.dacidian.net/" target="_blank">给力大辞典</a></div>
+<?php
+phpinfo();
+?>
+<div style="display:none"><script src="http://s14.cnzz.com/stat.php?id=2262423&web_id=2262423" language="JavaScript"></script>
+</div>
